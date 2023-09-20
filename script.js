f11ee234b79efcee994099c00e43ce6cb64c69a3
Daniela Del Valle <daniela.delvalle@outlook.com.ar>
2023-09-19 23:08:12 -0300

    Proyecto terminado

---
+++ script.js
@@ -0,0 +1,12 @@
+var encabezado = document.getElementById("mostrarFormulario");
+var formulario = document.getElementById("contacto-formulario");
+var titulo = document.getElementById("titulo-contacto");
+
+function mostrarFormulario () {
+    encabezado.style.display = 'none';
+    formulario.style.display = 'flex';
+    titulo.style.display = 'none';
+
+}
+
+
